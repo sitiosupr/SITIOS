aa5285a0f95d8c16815717b5c1fdb78b8f00d89b
Carolina Perez <carolinapm354@gmail.com>
2024-10-10 23:15:04 -0400

    Last Update Before Transfer of Ownership_map_10_9_2 is the latest version

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import json
 
 # Load the GeoJSON file
-with open('ASSETS/50pts.geojson', 'r') as f:
+with open('https://storage.googleapis.com/carolina_test/50pts.geojson', 'r') as f:
     geojson_data = json.load(f)
 
 # Initialize an empty list to store the project numbers
@@ -9,10 +9,18 @@ project_numbers = []
 
 # Loop through each feature in the GeoJSON
 for feature in geojson_data['features']:
-    # Extract the "Proyect Number" from the properties and add it to the list
+   # Extract relevant fields from the properties
     project_number = feature['properties'].get('Proyect Number')
+    project_name = feature['properties'].get('Name', 'Unknown Project')
+    project_description = feature['properties'].get('Description', 'No description available.')
+    
+    # Add the project info to the list
     if project_number:
-        project_numbers.append(project_number)
-
+        project_numbers.append({
+            'project_number': project_number,
+            'name': project_name,
+            'description': project_description,
+            'coordinates': feature['geometry']['coordinates']
+        })
 # Print the list of project numbers
 print(project_numbers)
